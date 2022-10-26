efc9173734b3bf837434660edfe5c6deaf91d300
DmitryKnyazkov <116336011+DmitryKnyazkov@users.noreply.github.com>
2022-10-26 22:28:30 +0300

    Update and rename Тема 2/Лабораторные работы/task1/main.py to Тема 2/Лабораторные работы/задача 1/main.py
    внес изменения копированием

---
+++ Тема 2/Лабораторные работы/задача 1/main.py
@@ -0,0 +1,13 @@
+list_ = [8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9]
+list_ = set (list_)
+
+
+
+sum_ = sum(list_)
+len_ = len(list_)
+srednee = sum_ / len_
+print(sum_)
+print(len_)
+print(round (srednee,5) )
+# TODO найти сумму, количество и среднее арифметическое уникальных чисел
+
