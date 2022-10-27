54c0e3fffeb99c0a21220b51caa2f76a381ec408
DmitryKnyazkov <116336011+DmitryKnyazkov@users.noreply.github.com>
2022-10-27 15:27:27 +0300

    копированием

--- Тема 3/Лабораторные работы/task9/main.py
+++ Тема 3/Лабораторные работы/task9/main.py
@@ -5,25 +5,19 @@ increase = 0.03  # рост цен
 
 need_money = 0  # количество денег, чтобы прожить 10 месяцев
 
-zp_za_desyc_mes = 10
-
+zp_za_desyc_mes = salary * 10
+spend_za_devy_mes = 0
 cikl = 0
-spend_za_dev_mes = 0
 spend_progres = spend
+
 while cikl != 9:
-    spend_progres = spend_progres + (spend_progres + increase)
-    spend_za_dev_mes = spend_progres + spend_za_dev_mes
+    spend_progres = spend_progres + (spend_progres * increase)
+    spend_za_devy_mes = spend_progres + spend_za_devy_mes
     cikl += 1
 
-spend_za_desyc = spend_za_dev_mes + spend
-need_money = spend_za_desyc -
-    zp_za_des_mes = salary * months
+spend_za_desyc_mes = spend_za_devy_mes + spend
 
-
-
-
-
-
+need_money = spend_za_desyc_mes - zp_za_desyc_mes
 # TODO Оформить решение
 
 print(round(need_money))
