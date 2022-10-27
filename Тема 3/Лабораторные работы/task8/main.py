c3bff1e725c30cdcf4816ddf05150b38a2146486
DmitryKnyazkov <116336011+DmitryKnyazkov@users.noreply.github.com>
2022-10-27 15:25:51 +0300

    копированием

--- Тема 3/Лабораторные работы/task8/main.py
+++ Тема 3/Лабораторные работы/task8/main.py
@@ -2,15 +2,15 @@ money_capital = 10000
 salary = 5000
 spend = 6000
 increase = 0.05
-
 month = 0  # количество месяцев, которое можно прожить
+bud = salary + money_capital # бюджет состоит из подушки и ЗП
 
-
-bud = salary + money_capital
 while bud >= spend:
     spend = spend + (spend * increase)
     money_capital = money_capital - (spend - salary)
     bud = salary + money_capital
     month += 1
+# TODO Оформить решение
 
-print(month-2)
+print(month - 2)  # вычел 2 для того чтобы совпадала со вторым правильным ответом
+
