da85f2bec3264ef04939825f6b99ebcab15f03cc
DmitryKnyazkov <116336011+DmitryKnyazkov@users.noreply.github.com>
2022-10-30 14:37:44 +0300

    копированием
    альтернатива через split и join

--- Тема 4/Самостоятельная работа/task1/main.py
+++ Тема 4/Самостоятельная работа/task1/main.py
@@ -1,7 +1,16 @@
 # TODO реализовать функцию
-def remove_whitespace():
-    ...
+def remove_whitespace(str_):
+    probel = " "
+    kolichestvo_probelov = 0
+    for znak in str_: # узнали максимальное количество пробелов, которое можем встретить в тексте
+        if znak == probel:
+            kolichestvo_probelov += 1
 
+    for n in range(kolichestvo_probelov, 1, -1): # убираем пробелы
+        probely = probel*n
+        str_ = str_.replace(probely, " ")
+
+    return str_
 
 str_with_space = """123.    test bks
 print   test11"""  # исходная строка
