13e4082d6a8a3de9393457942eda2ff9b3a6d08a
DmitryKnyazkov <116336011+DmitryKnyazkov@users.noreply.github.com>
2022-11-05 15:42:20 +0300

    Самостоятельная работа 5 task4

--- Тема 5/Самостоятельная работа/task4/main.py
+++ Тема 5/Самостоятельная работа/task4/main.py
@@ -1,3 +1,4 @@
+import random
 from random import choice
 
 
@@ -5,11 +6,26 @@ EAGLE = "Орел"
 TAILS = "Решка"
 
 coin = [EAGLE, TAILS]  # монета, для которой нужно выбрать случайную сторону
-counts = [10, 100, 1000, 100000, 1000000]  # различное количество подбрасываний
+counts = [10, 100, 1000, 10000]  # различное количество подбрасываний
 list_freq = []  # список, где будем хранить отношение количества выпавших орлов к решке
+list_result = []
 
 for count in counts:
-    ...  # TODO подсчитать количество выпаданий орлов и решек
+    for _ in range(count):
+        list_result.append(random.choice(coin))
+        skolko_orlov = list_result.count("Орел")
+        #print(skolko_orlov)
+        skolko_reshka = list_result.count("Решка")
+    print(list_result)
+
+    if skolko_reshka < skolko_orlov:
+        result = skolko_reshka / skolko_orlov
+    else:
+        result = skolko_orlov / skolko_reshka
+    list_freq.append(result)
+
+
+    # TODO подсчитать количество выпаданий орлов и решек
 
     # TODO разделить минимальное число среди орлов и решек на максимальное число и сохранить результат
 
