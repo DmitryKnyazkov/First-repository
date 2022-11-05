ab0d4870c4fab8521ce908ea24f18c8ca3f71d57
DmitryKnyazkov <116336011+DmitryKnyazkov@users.noreply.github.com>
2022-11-05 15:38:57 +0300

    Лабораторная работа 5 task5

--- Тема 5/Лабораторная работа/task5/main.py
+++ Тема 5/Лабораторная работа/task5/main.py
@@ -1,5 +1,14 @@
-def get_random_password() -> str:
-    ...  # TODO написать функцию генерации случайных паролей
+import random
+import string
+
+str_ = string.digits + string.ascii_uppercase + string.ascii_lowercase
+
+def get_random_password(n=8) -> str:
+    list_ = random.sample(str_, n)
+    return "".join(list_)
+
+     # TODO написать функцию генерации случайных паролей
 
 
-print(get_random_password())
+print(get_random_password(15))
+
