c5300b9f0eb2c003785662be53106fddfb9db502
DmitryKnyazkov <116336011+DmitryKnyazkov@users.noreply.github.com>
2022-11-05 15:36:14 +0300

    Лабораторная 5 task3

--- Тема 5/Лабораторная работа/task3/main.py
+++ Тема 5/Лабораторная работа/task3/main.py
@@ -1,6 +1,13 @@
-def get_unique_list_numbers() -> list[int]:
-    ...  # TODO написать функцию для получения списка уникальных целых чисел
+import random
 
+def get_unique_list_numbers() -> list[int]:
+    list_ = []
+    while len(list_) != 15:
+        chislo = random.randint(-10, 10)
+        if chislo not in list_:
+            list_.append(chislo)
+    return list_
+ # TODO написать функцию для получения списка уникальных целых чисел
 
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
