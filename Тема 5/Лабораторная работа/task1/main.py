237d795136b933f5168abe446ed104936330571a
DmitryKnyazkov <116336011+DmitryKnyazkov@users.noreply.github.com>
2022-11-05 15:34:17 +0300

    лабораторная 5 задание 1

--- Тема 5/Лабораторная работа/task1/main.py
+++ Тема 5/Лабораторная работа/task1/main.py
@@ -1,1 +1,12 @@
 # TODO решить с помощью list comprehension и распечатать его
+import pprint
+
+chislo = 16
+list_dict = list(range(chislo))
+def smena_znach(chislo=16):
+    for i in list_dict:
+        list_dict[i] = {'bin': bin(i), 'dec': i, 'hex': hex(i), 'oct': oct(i)}
+    return list_dict
+
+pprint.pprint(smena_znach(chislo))
+
